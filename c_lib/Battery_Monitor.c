bad9716b5df7d293731c2fd7472fbd8bbea88ebd
ptaenzer <ptaenzer@mymail.mines.edu>
2021-03-11 19:17:06 -0700

    fix attempt

--- c_lib/Battery_Monitor.c
+++ c_lib/Battery_Monitor.c
@@ -5,8 +5,8 @@
 
 static const float BITS_TO_BATTERY_VOLTS = 2*2.56/1023;
 
-static Filter_Data_t Battery_Filter;
-bool first_voltage;
+//static Filter_Data_t Battery_Filter;
+//bool first_voltage;
 
 /**
  * Function Battery_Monitor_Init initializes the Battery Monitor to record the current battery voltages.
@@ -19,11 +19,11 @@ void Battery_Monitor_Init()
     ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); 	// divider value of 128
     ADMUX |= (1 << REFS0) | (1 << REFS1); 			// AVcc w/ external capacitor on AREF
     // TODO Need to determine numerator and denominator coefficients
-    int filter_order = 4;
-    float numerator[] = {0.0, 0.091646653142, 0.353160775308, 0.131207565573, 0.004796803973};  
-    float denominator[] = {1.0, -0.627579402049, 0.267733142186, -0.066736038563, 0.007394096422};
-    Filter_Init(&Battery_Filter, numerator, denominator, filter_order+1);
-    first_voltage = true;
+    //int filter_order = 4;
+    //float numerator[] = {0.0, 0.091646653142, 0.353160775308, 0.131207565573, 0.004796803973};  
+    //float denominator[] = {1.0, -0.627579402049, 0.267733142186, -0.066736038563, 0.007394096422};
+    //Filter_Init(&Battery_Filter, numerator, denominator, filter_order+1);
+    //first_voltage = true;
 }
 
 /**
@@ -40,6 +40,7 @@ float Battery_Voltage()
     //data.split.MSB = ADCH;
     //voltage = Filter_Value(&Battery_Filter, data.value);
     //return data.value * BITS_TO_BATTERY_VOLTS;
+    
     float voltage;
     unsigned char sreg;
     sreg = SREG;
@@ -49,17 +50,18 @@ float Battery_Voltage()
         if (ADCSRA & (1 << ADIF)) {
     		data.split.LSB = ADCL;
     		data.split.MSB = ADCH;
-		//break;
-		sei();	// re-enable interrupts
+            //break;
+            sei();	// re-enable interrupts
     		SREG = sreg;
-		break;
-	}
+            break;
+        }
     }
     voltage = data.value * BITS_TO_BATTERY_VOLTS;
-    if (first_voltage) {
-	    Filter_SetTo(&Battery_Filter, voltage);
-	    first_voltage = false;
-    }
-    return Filter_Value(&Battery_Filter, voltage);
+    return voltage;
+    //if (first_voltage) {
+	    //Filter_SetTo(&Battery_Filter, voltage);
+	    //first_voltage = false;
+    //}
+    //return Filter_Value(&Battery_Filter, voltage);
 }
 
